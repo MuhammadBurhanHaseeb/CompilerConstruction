8afa3122854458c8adc1461ec0c8dd94aa94bb99
Muhammad Burhan <muhammadburhanhaseeb@gmail.com>
2024-11-09 11:08:22 +0500

    For  While Switch Function Statements Parsing
    Completed parsing module for for, while, switch, and function declarations.
    Ensures proper syntax handling with no syntax errors detected.
    Verified seamless integration with the tokenization module, accurately identifying and parsing control flow and function tokens.

--- Initial Compiler/parser.cpp
+++ Initial Compiler/parser.cpp
@@ -8,12 +8,12 @@ using namespace std;
 enum TokenType {
     T_CHAR_LITERAL , T_STRING_LITERAL,
     T_STRING, T_FLOAT, T_DOUBLE, T_CHAR, T_VOID, T_BOOL, T_INT,
-     T_FOR, T_WHILE, T_SWITCH, T_CASE, T_BREAK,
+     T_FOR, T_WHILE, T_SWITCH, T_CASE, T_BREAK,T_DEFAULT,
     T_ID, T_NUM, T_IF, T_ELSE, T_RETURN, 
     T_ASSIGN, T_PLUS, T_MINUS, T_MUL, T_DIV, 
     T_LPAREN, T_RPAREN, T_LBRACE, T_RBRACE,  
-    T_SEMICOLON, T_GT,T_LT,T_LE,T_GE,T_NEQ,T_EQ ,T_EOF, T_COLON,
-    T_AGAR , T_MAGAR,
+    T_SEMICOLON, T_GT,T_LT,T_LE,T_GE,T_NEQ,T_EQ ,T_EOF, T_COLON,T_COMMA,
+    T_AGR , T_MAGR,
     T_INCREMENT,T_DECREMENT,   
       
 };
@@ -66,12 +66,13 @@ public:
                 else if (word == "else") tokens.push_back(Token{T_ELSE, word, lineNo, colNo});
                 else if (word == "return") tokens.push_back(Token{T_RETURN, word, lineNo, colNo});
 
-                else if (word == "agr") tokens.push_back(Token{T_AGAR, word, lineNo, colNo});
-                else if (word == "magr") tokens.push_back(Token{T_MAGAR, word, lineNo, colNo});
+                else if (word == "agr") tokens.push_back(Token{T_AGR, word, lineNo, colNo});
+                else if (word == "magr") tokens.push_back(Token{T_MAGR, word, lineNo, colNo});
 
                 else if (word == "for") tokens.push_back(Token{T_FOR, word, lineNo, colNo});       
                 else if (word == "while") tokens.push_back(Token{T_WHILE, word, lineNo, colNo});   
-                else if (word == "switch") tokens.push_back(Token{T_SWITCH, word, lineNo, colNo}); 
+                else if (word == "switch") tokens.push_back(Token{T_SWITCH, word, lineNo, colNo});
+                else if (word == "default") tokens.push_back(Token{T_DEFAULT, word, lineNo, colNo}); 
                 else if (word == "case") tokens.push_back(Token{T_CASE, word, lineNo, colNo});    
                 else if (word == "break") tokens.push_back(Token{T_BREAK, word, lineNo, colNo});   
 
@@ -96,6 +97,7 @@ public:
                 case '}': tokens.push_back(Token{T_RBRACE, "}", lineNo, colNo}); break;
                 case ';': tokens.push_back(Token{T_SEMICOLON, ";", lineNo, colNo}); break;
                 case ':': tokens.push_back(Token{T_COLON, ":", lineNo, colNo}); break;
+                case ',': tokens.push_back(Token{T_COMMA, ",", lineNo, colNo}); break;
                 case '=':
                     current = src[pos+1];
                     if (current == '=') {
@@ -246,8 +248,12 @@ private:
         else if (tokens[pos].type == T_FLOAT ) {
             parseDeclaration( T_FLOAT );
         }
-        else if (tokens[pos].type == T_VOID  ) {
-            parseDeclaration( T_VOID );
+        else if ((tokens[pos].type == T_VOID  || tokens[pos].type == T_INT ||
+                 tokens[pos].type == T_STRING  || tokens[pos].type == T_BOOL ||
+                 tokens[pos].type == T_FLOAT  || tokens[pos].type == T_DOUBLE  ||
+                 tokens[pos].type == T_CHAR) && tokens[pos+2 ].type == T_LPAREN ) {
+
+                parseFunctionStatement();
         }
         else if (tokens[pos].type == T_STRING  ) {
             parseDeclaration( T_STRING );
@@ -259,12 +265,22 @@ private:
             parseDeclaration( T_BOOL );
         }
         else if (tokens[pos].type == T_ID) {
-            parseAssignment();
+            if(tokens[pos+1].type == T_INCREMENT )
+            {
+                parseIncrementAssignment();
+            }
+            else if (tokens[pos +1].type == T_DECREMENT )
+            {
+                parseDecrementAssignment();
+            }
+            else{
+                parseAssignment();
+            }
         } else if (tokens[pos].type == T_IF) {
             parseIfStatement();
         }
-        else if (tokens[pos].type == T_AGAR) {
-            parseAgarStatement();
+        else if (tokens[pos].type == T_AGR) {
+            parseAgrStatement();
         } else if (tokens[pos].type == T_RETURN) {
             parseReturnStatement();
         } else if (tokens[pos].type == T_LBRACE) {  
@@ -298,7 +314,31 @@ private:
         expect(T_SEMICOLON);
     }
 
+    void VoidFunctionDeclaration()
+    {
+        expect(T_VOID);
+        expect(T_ID);
+
+    }
+
     void parseAssignment( ) {
+        expect(T_ID);
+        expect(T_ASSIGN);
+        parseExpression();
+        expect(T_SEMICOLON);
+    }
+    void parseIncrementAssignment( ) {
+        expect(T_ID);
+        expect(T_INCREMENT);
+        expect(T_SEMICOLON);
+    }
+    void parseDecrementAssignment( ) {
+        expect(T_ID);
+        expect(T_DECREMENT);
+        expect(T_SEMICOLON);
+    }
+     void parseDeclarationAssignment(TokenType a ) {
+        expect(a);
         expect(T_ID);
         expect(T_ASSIGN);
         parseExpression();
@@ -316,14 +356,14 @@ private:
             parseStatement();  
         }
     }
-    void parseAgarStatement() {
-        expect(T_AGAR);
+    void parseAgrStatement() {
+        expect(T_AGR);
         expect(T_LPAREN);
         parseExpression();
         expect(T_RPAREN);
         parseStatement();  
-        if (tokens[pos].type == T_MAGAR) {
-            expect(T_MAGAR);
+        if (tokens[pos].type == T_MAGR) {
+            expect(T_MAGR);
             parseStatement();  
         }
     }
@@ -335,13 +375,11 @@ private:
     }
 
      void parseForStatement() {
-        cout << "Parsing for statement" << endl;
         expect(T_FOR);
         expect(T_LPAREN);
     
         // Parse the initialization statement (e.g., int i = 0)
-        parseDeclaration(T_INT);  // Assumes an `int` variable declaration, can extend if needed
-
+        parseDeclarationAssignment(T_INT);
         // Parse the condition (e.g., i < 5)
         parseExpression();
         expect(T_SEMICOLON);
@@ -353,92 +391,174 @@ private:
         parseBlock();  // Parse the body of the `for` loop
     }
 
-    void parseIncrementDecrement() {
-        expect(T_ID);  // The iterator variable (e.g., i)
-        if (tokens[pos].type == T_PLUS || tokens[pos].type == T_MINUS) {
-            TokenType op = tokens[pos].type;
-            pos++;
-            if (tokens[pos].type == op) {  // Expect `++` or `--`
-                pos++;
-            } else {
-                cout << "Syntax error: expected '++' or '--' at line " << tokens[pos].lineNo << ", column " << tokens[pos].colNo << endl;
-                exit(1);
-            }
-        } else {
-            cout << "Syntax error: expected increment/decrement operator at line " << tokens[pos].lineNo << ", column " << tokens[pos].colNo << endl;
-            exit(1);
-        }
+   void parseIncrementDecrement() {
+    expect(T_ID);  // Expect the iterator variable (e.g., i)
+
+    if (tokens[pos].type == T_INCREMENT) {  // Check for `++`
+        pos++;  // Move past the `++` token
+    } else if (tokens[pos].type == T_DECREMENT) {  // Check for `--`
+        pos++;  // Move past the `--` token
+    } else {
+        cout << "Syntax error: expected increment (++) or decrement (--) operator at line "
+             << tokens[pos].lineNo << ", column " << tokens[pos].colNo << endl;
+        exit(1);
     }
+}
+
 
     void parseWhileStatement() {
         cout << "Parsing while statement" << endl;
-        pos++;
+        expect(T_WHILE);
         expect(T_LPAREN);
-        expect(T_ID); // Placeholder for condition
+        parseExpression();  // Parse the condition inside parentheses
         expect(T_RPAREN);
-        expect(T_LBRACE);
-        expect(T_RBRACE);
+        cout<<"hello "<<endl;
+        parseBlock();  // Parse the body of the while loop
     }
 
-    void parseSwitchStatement() {
-        cout << "Parsing switch statement" << endl;
-        pos++;
-        expect(T_LPAREN);
-        expect(T_ID); // Placeholder for switch variable
-        expect(T_RPAREN);
-        expect(T_LBRACE);
-        // Process cases
-        while (tokens[pos].type == T_CASE) {
+void parseSwitchStatement() {
+    cout << "Parsing switch statement" << endl;
+    pos++;
+    expect(T_LPAREN);
+    expect(T_ID); // Switch variable (or expression)
+    expect(T_RPAREN);
+    expect(T_LBRACE);
+
+    // Process cases
+    while (tokens[pos].type == T_CASE || tokens[pos].type == T_DEFAULT) {
+        if (tokens[pos].type == T_CASE) {
             parseCaseStatement();
+        } else if (tokens[pos].type == T_DEFAULT) {
+            parseDefaultStatement();
         }
-        expect(T_RBRACE);
     }
 
-    void parseCaseStatement() {
-        cout << "Parsing case statement" << endl;
+    expect(T_RBRACE); // End of switch block
+}
+
+void parseCaseStatement() {
+    cout << "Parsing case statement" << endl;
+    pos++;
+    expect(T_NUM); // Case value (like case 1:)
+    expect(T_COLON);
+
+    // Parsing the case body: This could be a sequence of statements.
+    while (tokens[pos].type != T_CASE && tokens[pos].type != T_DEFAULT && tokens[pos].type != T_RBRACE) {
+        parseStatement(); // Assume parseStatement handles a single statement
+    }
+}
+
+void parseDefaultStatement() {
+    cout << "Parsing default statement" << endl;
+    pos++;
+    expect(T_COLON);
+
+    // Parsing the default body: This could be a sequence of statements.
+    while (tokens[pos].type != T_CASE && tokens[pos].type != T_DEFAULT && tokens[pos].type != T_RBRACE) {
+        parseStatement(); // Parsing a single statement
+    }
+}
+
+void parseFunctionStatement() {
+    // Parse the return type (e.g., int, void, etc.)
+    if (tokens[pos].type == T_INT || tokens[pos].type == T_DOUBLE || tokens[pos].type == T_FLOAT || 
+        tokens[pos].type == T_VOID || tokens[pos].type == T_STRING || tokens[pos].type == T_CHAR || 
+        tokens[pos].type == T_BOOL) {
+        
+        pos++;  // Advance past the return type
+    } else {
+        cout << "Syntax error: expected return type at line " << tokens[pos].lineNo << ", column " << tokens[pos].colNo << endl;
+        exit(1);
+    }
+
+    // Parse the function name (identifier)
+    expect(T_ID);
+
+    // Parse the parameter list
+    expect(T_LPAREN);
+    if (tokens[pos].type != T_RPAREN) {  // If not immediately closing, parse parameters
+        parseParameterList();
+    }
+    expect(T_RPAREN);
+
+    // Parse the function body
+    parseBlock();  // Assuming parseBlock handles statements within curly braces
+}
+
+void parseParameterList() {
+    // Parse parameter type and identifier (e.g., int x, float y)
+    parseParameter();
+    while (tokens[pos].type == T_COMMA) {  // Support for multiple parameters separated by commas
         pos++;
-        expect(T_NUM); // Placeholder for case value
-        expect(T_COLON); // Assuming a colon token type
-        expect(T_ID); // Placeholder for case body
-        expect(T_SEMICOLON);
+        parseParameter();
     }
+}
 
+void parseParameter() {
+    // Expect a type and identifier for each parameter (e.g., int x)
+    if (tokens[pos].type == T_INT || tokens[pos].type == T_DOUBLE || tokens[pos].type == T_FLOAT || 
+        tokens[pos].type == T_STRING || tokens[pos].type == T_CHAR || tokens[pos].type == T_BOOL) {
+        
+        pos++;  // Advance past the type
+    } else {
+        cout << "Syntax error: expected parameter type at line " << tokens[pos].lineNo << ", column " << tokens[pos].colNo << endl;
+        exit(1);
+    }
+    expect(T_ID);  // Expect parameter name
+}
     void parseBreakStatement() {
         cout << "Parsing break statement" << endl;
         pos++;
         expect(T_SEMICOLON);
     }
 
-
-    void parseExpression() {
+void parseExpression() {
+    parseTerm();
+    
+    // Handle addition, subtraction, and relational operators in expression
+    while (tokens[pos].type == T_PLUS || tokens[pos].type == T_MINUS || 
+           tokens[pos].type == T_GT || tokens[pos].type == T_LT ||
+           tokens[pos].type == T_GE || tokens[pos].type == T_LE || 
+           tokens[pos].type == T_EQ || tokens[pos].type == T_NEQ) {
+        
+        pos++;  // Advance past the operator
         parseTerm();
-        while (tokens[pos].type == T_PLUS || tokens[pos].type == T_MINUS) {
-            pos++;
-            parseTerm();
-        }
-        if (tokens[pos].type == T_GT) {
-            pos++;
-            parseExpression();  // After relational operator, parse the next expression
-        }
     }
-
-    void parseTerm() {
-        parseFactor();
-        while (tokens[pos].type == T_MUL || tokens[pos].type == T_DIV) {
-            pos++;
-            parseFactor();
-        }
-    }
-
-   
-    void parseFactor() {
-    if (tokens[pos].type == T_NUM || tokens[pos].type == T_ID ||
-     tokens[pos].type == T_STRING_LITERAL || tokens[pos].type == T_CHAR_LITERAL ) {
+}
+void parseTerm() {
+    parseFactor();
+    
+    // Handle multiplication and division operators
+    while (tokens[pos].type == T_MUL || tokens[pos].type == T_DIV) {
         pos++;
+        parseFactor();
+    }
+}
+
+void parseFactor() {
+    if (tokens[pos].type == T_NUM || tokens[pos].type == T_ID ||
+        tokens[pos].type == T_STRING_LITERAL || tokens[pos].type == T_CHAR_LITERAL) {
+        
+        string id = tokens[pos].value;  // Store the identifier or literal value
+        pos++;  // Advance past literals or identifiers
+        
+        // Check for increment (++) and decrement (--) after an identifier
+        if (tokens[pos].type == T_INCREMENT) {  // If it's increment
+            pos++;  // Move past the `++`
+            cout << "Incrementing " << id << endl;  // Handle incrementing logic
+        } else if (tokens[pos].type == T_DECREMENT) {  // If it's decrement
+            pos++;  // Move past the `--`
+            cout << "Decrementing " << id << endl;  // Handle decrementing logic
+        }
     } else if (tokens[pos].type == T_LPAREN) {
         expect(T_LPAREN);
         parseExpression();
         expect(T_RPAREN);
+        
+    } else if (tokens[pos].type == T_ASSIGN) {
+        pos++;  // Move past the assignment
+        parseExpression();  // Expect an expression after `=`
+
     } else {
         cout << "Syntax error: unexpected token '" << tokens[pos].value 
              << "' at line " << tokens[pos].lineNo 
@@ -446,6 +566,12 @@ private:
         exit(1);
     }
 }
+
+
+
+
+
+
 void expect(TokenType type) {
     if (tokens[pos].type == type) {
         pos++;
@@ -472,6 +598,8 @@ string tokenTypeToString(TokenType type) {
         case T_NUM: return "T_NUM";
         case T_IF: return "T_IF";
         case T_ELSE: return "T_ELSE";
+        case T_AGR: return "T_AGR";
+        case T_MAGR: return "T_MAGR";
         case T_RETURN: return "T_RETURN";
         case T_ASSIGN: return "T_ASSIGN";
         case T_PLUS: return "T_PLUS";
@@ -499,7 +627,6 @@ int main() {
         char ammad  ; 
         float dd;
         dd = 2.5 ; 
-        void a ;
         double ajmal ; 
         bool hello ; 
         char ammad  ; 
@@ -513,11 +640,13 @@ int main() {
         } else {
             return 0;
         }
-        agar (b > 10) {
+
+        agr (b > 10) {
             return b;
-        } magar {
+        } magr {
             return 0;
         }
+        
 
 
         for (int i = 0; i < 5 ; i++) {
@@ -531,6 +660,7 @@ int main() {
         while (count < 5) {
             count++;
         }
+
         int num ;
         num = 2;
         switch (num) {
@@ -544,23 +674,23 @@ int main() {
             
             break;
         default:
+            break ;
             
         }
 
+        
         void printData()
         {
-        
+
         }
-
-
 
     )";
 
     Lexer lexer(input);
     vector<Token> tokens = lexer.tokenize();
     
-    // Parser parser(tokens);
-    // parser.parseProgram();
+    Parser parser(tokens);
+    parser.parseProgram();
 
     return 0;
 }
